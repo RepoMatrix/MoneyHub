096f7634052b5635920ea588acf400fec933cebb
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-19 14:46:58 -0700

    Add the Overview info container content

--- src/features/overview/Overview.tsx
+++ src/features/overview/Overview.tsx
@@ -4,7 +4,20 @@ export const Overview = () => {
   return (
     <>
       <h1 className="overview-title">Overview</h1>
-      <div className="overview-info-container"></div>
+      <div className="overview-info-container">
+        <div>
+          <h4>Current Balance</h4>
+          <p>$4,836.00</p>
+        </div>
+        <div>
+          <h4>Income</h4>
+          <p>$3,814,25</p>
+        </div>
+        <div>
+          <h4>Expenses</h4>
+          <p>$1,700.50</p>
+        </div>
+      </div>
     </>
   )
 }
